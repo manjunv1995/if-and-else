61ed77b546227d16f3ceaa7bca6c883c6a138ad9
manjunath vijapuram <vmmanjunathece@gmail.com>
2019-11-19 08:19:02 +0530

    bug fixed

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 function dealWith() {
 
-    let names = ['manju', 'ram', 'pavan'];
-    names.forEach(function getName(el, index) {
-        document.getElementById("print").innerHTML = (`${index+1}: Hello Mister ${el}`);
-        // document.write(`${index+1 } hello mister ${el}<br/>`); // second way
+    let names = ['Manju', 'Ram', 'Pavan', 'Hari'];
+    names.forEach((el, index) => {
+
+        document.getElementById(`print-${index+1}`).innerHTML = (`${index+1}: Hello Mister ${el}`);
     })
 }
